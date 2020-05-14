92e4eb96e93dfa8eb96b198f16d0b226d38b3797
Jean <nguyen.jean@me.com>
2020-05-14 18:43:39 +0200

    factory the nav js code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,19 +5,15 @@ const MenuLine = document.getElementById("Menu-line");
 const MenuLink = document.getElementById("Menu-link");
 const MenuButton = document.getElementById("Menu-button");
 
-console.log(MenuButton);
-
 //MenuButton.removeChild(MenuButton.childNodes[O]);
 
-MenuLine.addEventListener("click", function() {
-  MenuLink.classList.toggle("hidden");
-  //MenuLink.classList.remove("hidden");
-  MenuLine.style.display = "none";
-  MenuCross.style.display = "block";
-});
-
-MenuCross.addEventListener("click", function() {
-  MenuLink.classList.add("hidden");
-  MenuLine.style.display = "block";
-  MenuCross.style.display = "none";
+MenuButton.addEventListener("click", function() {
+  const MenuToggle = MenuLink.classList.toggle("hidden");
+  if (MenuToggle === false) {
+    MenuLine.style.display = "none";
+    MenuCross.style.display = "block";
+  } else {
+    MenuLine.style.display = "block";
+    MenuCross.style.display = "none";
+  }
 });
