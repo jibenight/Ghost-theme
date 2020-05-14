a77d0c2ce1a7889bd6a8c485b790a6519da5e77c
Jean <nguyen.jean@me.com>
2020-05-14 18:19:13 +0200

    resolve display:none issue

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,15 +3,21 @@
 const MenuCross = document.getElementById("Menu-cross");
 const MenuLine = document.getElementById("Menu-line");
 const MenuLink = document.getElementById("Menu-link");
+const MenuButton = document.getElementById("Menu-button");
+
+console.log(MenuButton);
+
+//MenuButton.removeChild(MenuButton.childNodes[O]);
 
 MenuLine.addEventListener("click", function() {
-  MenuLink.style.display = "block";
+  MenuLink.classList.toggle("hidden");
+  //MenuLink.classList.remove("hidden");
   MenuLine.style.display = "none";
   MenuCross.style.display = "block";
 });
 
 MenuCross.addEventListener("click", function() {
-  MenuLink.style.display = "none";
+  MenuLink.classList.add("hidden");
   MenuLine.style.display = "block";
   MenuCross.style.display = "none";
 });
